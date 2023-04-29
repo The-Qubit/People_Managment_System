aab72f8599656c4b3deca1d1b370804d89428131
cubernetes <timo42@protonmail.com>
2023-04-29 23:59:52 +0200

    Add basic untested backend/main.py

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pyodbc
+
+def main():
+    server = 'localhost:1433'
+    database = 'master'
+    username = 'sa'
+    password = 'User Retrain Shelve Showy Goatskin Dowry Twelve Petunia'
+
+    conn = pyodbc.connect(f'
+      DRIVER={{ODBC Driver 17 for SQL Server}};
+      SERVER={server};
+      DATABASE={database};
+      UID={username};
+      PWD={password}'
+    )
+
+    new_db_name = 'test'
+    cursor.execute(f'CREATE DATABASE {new_db_name}')
+
+    conn.commit()
+
+    conn.close()
+
+if __name__ == '__main__':
+    sys.exit(main())
