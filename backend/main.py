dc59bd89cac6e90546411cc86bd4c6d41e999fad
cubernetes <timo42@protonmail.com>
2023-04-30 11:37:38 +0200

    Make backend/main.py work

--- backend/main.py
+++ backend/main.py
@@ -5,24 +5,27 @@ import sys
 import pyodbc
 
 def main():
-    server = 'localhost'
-    database = 'master'
+    server = 'host.docker.internal'
+    database = 'test'
     username = 'sa'
     password = 'Survive_Hertz_Massager_Rage_Creative_Grievous_Mossy_Purify144!'
 
-    conn = pyodbc.connect(f'''
-      DRIVER={{ODBC Driver 17 for SQL Server}};
-      SERVER={server};
-      DATABASE={database};
-      UID={username};
-      PWD={password}'''
+    conn = pyodbc.connect(
+      driver='{ODBC Driver 17 for SQL Server}',
+      host=server,
+      database=database,
+      user=username,
+      password=password,
     )
+
     cursor = conn.cursor()
 
-    new_db_name = 'test'
-    cursor.execute(f'CREATE DATABASE {new_db_name}')
+    cursor.execute(f'SELECT * FROM test1;')
 
-    conn.commit()
+    for row in cursor.fetchall():
+        print(tuple(row))
+
+    # conn.commit()
 
     conn.close()
 
